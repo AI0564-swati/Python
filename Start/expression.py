7e732e0d9d4926288d9223bedd608f21e1f6cb07
AI0564-swati <swati@aggregateintelligence.in>
2024-03-11 17:35:39 +0530

    feat: file

--- Start/expression.py
+++ Start/expression.py
@@ -1,31 +1,31 @@
-''' operator precedence : 
-not > and > or
-(), **, *, /, //(Integer Division, i.e., floor - closest lesser integer value), %, (+, - are operated left to right) '''
+# ''' operator precedence : 
+# not > and > or
+# (), **, *, /, //(Integer Division, i.e., floor - closest lesser integer value), %, (+, - are operated left to right) '''
 
-a = 3
-b = 2
-sum = a + b
-print(sum)
+# a = 3
+# b = 2
+# sum = a + b
+# print(sum)
 
-diff = a - b
-print(diff)
+# diff = a - b
+# print(diff)
 
-# String and Numeric values can operate together with * (Repetation)
-A,B = 2,3
-Txt = "@"
-print(A*Txt) 
-print(A*Txt*B) 
+# # String and Numeric values can operate together with * (Repetation)
+# A,B = 2,3
+# Txt = "@"
+# print(A*Txt) 
+# print(A*Txt*B) 
 
-# String and Numeric values can operate together with + (Concatenation)
-A,B = "2",3
-Txt = "@"
-print((A+Txt)) 
-print((A+Txt)*B) 
+# # String and Numeric values can operate together with + (Concatenation)
+# A,B = "2",3
+# Txt = "@"
+# print((A+Txt)) 
+# print((A+Txt)*B) 
 
-# Arthematic expression with integer and float will result in float
-print(3+5.0) # 8.0
-print(1/2) # 0.5
-print(1//2) # 0.0
-print(6/2) # 3.0
+# # Arthematic expression with integer and float will result in float
+# print(3+5.0) # 8.0
+# print(1/2) # 0.5
+# print(1//2) # 0.0
+# print(6/2) # 3.0
 
-# modulo (%) will give -ve remainder only in case numerator is +ve and denominatior is -ve, in other case it will always be +ve.
+# # modulo (%) will give -ve remainder only in case numerator is +ve and denominatior is -ve, in other case it will always be +ve.

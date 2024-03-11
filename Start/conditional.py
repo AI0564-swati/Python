ec5e103559541afa33c2055d2d5ec2b904516c8e
AI0564-swati <swati@aggregateintelligence.in>
2024-03-11 17:34:08 +0530

    update

--- Start/conditional.py
+++ Start/conditional.py
@@ -1,67 +1,67 @@
-light = input("light : ")
+# light = input("light : ")
 
-if(light == "red"):
-    print("stop")
-elif(light == "yellow"):
-    print("look")
-elif(light == "green"):
-    print("go")
-else:
-    print("light is broken")
+# if(light == "red"):
+#     print("stop")
+# elif(light == "yellow"):
+#     print("look")
+# elif(light == "green"):
+#     print("go")
+# else:
+#     print("light is broken")
 
-# Ternary operator
+# # Ternary operator
 
-food = input("food : ")
-eat = "Yes" if food == "cake" or food == "jalebi" else "no"
-print(eat)
+# food = input("food : ")
+# eat = "Yes" if food == "cake" or food == "jalebi" else "no"
+# print(eat)
 
-# Clever If/Ternary Operator
-age = int(input("age : "))
-vote = ("Yes", "No") [age < 18]
-print(vote)
+# # Clever If/Ternary Operator
+# age = int(input("age : "))
+# vote = ("Yes", "No") [age < 18]
+# print(vote)
 
-num1 = int(input("num1 : "))
-num2 = int(input("num2 : "))
+# num1 = int(input("num1 : "))
+# num2 = int(input("num2 : "))
 
-print(num1 >= num2)
+# print(num1 >= num2)
 
-marks = int(input("Enter your marks : "))
+# marks = int(input("Enter your marks : "))
 
-if(marks >= 90):
-    if(marks > 100):
-        print("Invalid marks, please enter marks less than 101")
-    grade = "A"
-elif(marks >= 80 and marks < 90):
-    grade = "B"
-elif(marks >= 70 and marks < 80):
-    grade = "C"
-else:
-    grade = "D"
+# if(marks >= 90):
+#     if(marks > 100):
+#         print("Invalid marks, please enter marks less than 101")
+#     grade = "A"
+# elif(marks >= 80 and marks < 90):
+#     grade = "B"
+# elif(marks >= 70 and marks < 80):
+#     grade = "C"
+# else:
+#     grade = "D"
 
-print("Grade : ", grade)
+# print("Grade : ", grade)
 
-no = int(input("Enter the number : "))
+# no = int(input("Enter the number : "))
 
-if(no%2==0):
-    print(no, "is even")
-else:
-    print(no, "is odd")
+# if(no%2==0):
+#     print(no, "is even")
+# else:
+#     print(no, "is odd")
 
-a = int(input("Enter the number1 : "))
-b = int(input("Enter the number2 : "))
-c = int(input("Enter the number3 : "))
+# a = int(input("Enter the number1 : "))
+# b = int(input("Enter the number2 : "))
+# c = int(input("Enter the number3 : "))
 
-if(a > b and a > c):
-    print(a, "is greatest")
-elif(b > a and b > c):
-    print(b, "is greatest")
-else:
-    print(c, "is greatest")
+# if(a > b and a > c):
+#     print(a, "is greatest")
+# elif(b > a and b > c):
+#     print(b, "is greatest")
+# else:
+#     print(c, "is greatest")
 
-no = int(input("Enter the number : "))
+# no = int(input("Enter the number : "))
 
-if(no%7==0):
-    print(no, "is divisible by 7")
-else:
-    print(no, "is not divisible by 7")
+# if(no%7==0):
+#     print(no, "is divisible by 7")
+# else:
+#     print(no, "is not divisible by 7")
 

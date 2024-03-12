423ac30a9231d56fba34a643e2bc11902a7c05c1
AI0564-swati <swati@aggregateintelligence.in>
2024-03-12 12:26:11 +0530

    file I/O

--- Start/file_io.py
+++ Start/file_io.py
@@ -52,34 +52,55 @@
 # os.remove("demo.txt")
 
 # Create a file and replace some content in it then search some content in it.
-with open("practise.txt", "w") as f:
-  f.write("Hi everyone!\nWe are learning file I/O\n")
-  f.write("using Java\nI like pragramming in Java.")
+# with open("practise.txt", "w") as f:
+#   f.write("Hi everyone!\nWe are learning file I/O\n")
+#   f.write("using Java\nI like pragramming in Java.")
 
-with open("practise.txt", "r")as f:
-  data = f.read()
+# with open("practise.txt", "r")as f:
+#   data = f.read()
 
-new_data = data.replace("Java", "Python")
-print(new_data)
+# new_data = data.replace("Java", "Python")
+# print(new_data)
 
-with open("practise.txt", "w")as f:
-  f.write(new_data)
+# with open("practise.txt", "w")as f:
+#   f.write(new_data)
 
-word = "learning"
-with open("practise.txt", "r") as f:
-  data = f.read()
-  if(data.find(word) != -1):
-    print("Found")
-  else:
-    print("Not found")
+# word = "learning"
+# with open("practise.txt", "r") as f:
+#   data = f.read()
+#   if(data.find(word) != -1):
+#     print("Found")
+#   else:
+#     print("Not found")
 
-word = "learning"
-data = True
-line = 1
-with open("practise.txt", "r") as f:
-  while data:
-    data = f.readline()
-    if(word in data): # similar to 
-      print("Found")
-    else:
-      print("Not found")
+# def check_for_line():
+#   word = "learning"
+#   data = True
+#   line = 1
+#   with open("practise.txt", "r") as f:
+#     while data:
+#       data = f.readline()
+#       if(word in data): # similar to data.find(word)
+#        print(line)
+#       else:
+#         line += 1
+#   return -1
+
+# print(check_for_line())
+
+# with open("num.txt", "w") as f:
+#     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#     numbers_as_strings = [str(num) for num in numbers]
+#     f.write(",".join(numbers_as_strings))
+
+# with open("num.txt", "r") as f:
+#     data = f.read()
+#     # print(data)
+
+#     num = ""
+#     for i in range(len(data)):
+#         if(data[i] == ","):
+#             print(int(num))
+#             num = ""
+#         else:
+#             num += data[i]

e4e70b0cb8dbd6218f12761031b403b2197fdc86
AI0564-swati <swati@aggregateintelligence.in>
2024-03-14 16:11:07 +0530

    day8

---
+++ Start/oops.py
@@ -0,0 +1,148 @@
+# Object-Oriented Programming System/Structure
+
+# Procedural --> Functional --> OOPS
+# This reduces redundancy and increases reusability.
+
+# Class is a blueprint for creating objects.
+
+# creating class
+# class Student:
+#   name = "Swati Tanu"
+
+# # creating object (instance)
+# s1 = Student()
+# print("s1 => ", s1)
+# print(s1.name)
+
+# Constructor
+# All classes have a function called _init_(), which is always executed when the object is being initiated. Python always execute this function automatically.
+
+# Default constructor
+# def __init__(self):
+#   pass
+
+# Parameterized constructor
+# class Car:
+#   ParentCompany = "Mercedes-Benz Group"
+#   def __init__(self, color, brand, engine):
+#     self.color = color
+#     self.brand = brand
+#     self.engine = engine
+#     print("self => ", self)
+#     print("Adding new car in database...")
+#   # color = "Black",
+#   # brand = "Mercedes"
+
+# print("ParentCompany ==> ", Car.ParentCompany) # We can access class attribute directly via class
+# car1 = Car("Black", "Mercedes", "OM904") 
+# print(car1.color, car1.brand, car1.engine) # attributes
+
+# i.) Attributes: 1. class.attr 2. obj.attr
+
+# obj attr > class attr (in usage/preferance)
+
+# ii.) Methods : Functions that belong to objects.
+
+# class Student:
+#   def __init__(self, name, marks):
+#     self.name = name
+#     self.marks = marks
+
+#   @staticmethod
+#   def hello():
+#     print("Using decorator to convert this to a static method, i.e., not using self parameter.")
+
+#   def get_avg(self):
+#     sum = 0
+#     for val in self.marks:
+#       sum += val
+#     print("Hi", self.name, ". Your avg score is:" , sum/3)
+
+# s1 = Student("Tony Stark", [87,94,77])
+# s1.get_avg()
+# s1.hello()
+
+# Static Methods: Methods that don't use self parameter. It works at class level.
+# @staticmethod # decorator
+
+# Decorator is a function that takes a function in parameter and returns a function in output too after changing it's behavior.
+
+# 4 pillars of OPPS:
+
+# 1. Abstraction
+# Hiding the implementation details of a class & only showing the essential features to the user. 
+# class Car:
+#   def __init__(self):
+#     self.acc = False
+#     self.brk = False
+#     self.clutch = False
+
+#   def start(self):
+#     self.clutch = True
+#     self.acc = True
+#     print("Car Started...")
+
+# car1 = Car()
+# car1.start()
+
+# 2. Encapsulation
+# Wrapping data and functions into a single unit (object).
+
+# 3. Inheritance
+# When one class(child/derived) derives the properties & methods of another class(parent/base)
+
+# 4. Polymorphism
+# When the same operator is allowed to have different meaning according to the context.
+# Eg:
+# print(1 + 2) # adds in case of numbers
+# print("Swati" + "Tanu") # concatenates in case of strings
+
+# class Account:
+#   def __init__(self, bal, acc):
+#     self.balance = bal
+#     self.account = acc
+
+#   def debit(self, amount):
+#     self.balance -= amount
+#     print("Rs.", amount, "was debited.")
+#     print("Total balance = ", self.get_balance())
+
+#   def credit(self, amount):
+#     self.balance += amount
+#     print("Rs.", amount, "was credited.")
+#     print("Total balance = ", self.get_balance())
+
+#   def get_balance(self):
+#     return self.balance
+
+# acc1 = Account(10000, 12345)
+# print(acc1.balance)
+# print(acc1.account)
+# acc1.debit(1000)
+# acc1.credit(500)
+
+# del keyword: Used to delete object properties or object itself
+
+# del s1.name
+# del s1
+
+# class Car:
+#   @staticmethod
+#   def start():
+#     print('Car started...')
+
+#   @staticmethod
+#   def stop():
+#     print('Car stopped...')
+
+# class ToyotaCar(Car):
+#   def __init__(self, name):
+#     self.name = name
+
+# car1 = ToyotaCar("Fortuner")
+# car2 = ToyotaCar("Prius")
+
+# print(car1.start())
+
+# super() method is used to access methods of the parent class.
+
---
+++ Training/day8.py
@@ -0,0 +1,135 @@
+# Web Scrapping
+
+# Used to extract content or data from a website.
+# Example: emails, location, business, flight data etc.
+# External module bs4, requests, lxml
+
+# Example 1: 
+# import requests
+# res = requests.get("https://en.wikipedia.org/wiki/Robots.txt")
+# print(res)
+# txt = res.text
+# print(txt)
+
+# Example 2: 
+# import requests
+# res = requests.get("https://analytics.usa.gov/data/live/browsers.json")
+# print(res)
+# txt = res.text
+# txt = res.json()
+# txt = (res.json()['totals']['browser'])
+# txt = (res.json()['query']['dateRanges'][0]['startDate'])
+# print(txt)
+
+# Optimized code: 
+
+# print(requests.get("https://en.wikipedia.org/wiki/Robots.txt").text)
+# print(requests.get("https://analytics.usa.gov/data/live/browsers.json").json()['totals']['browser'])
+# print(requests.get("https://analytics.usa.gov/data/live/browsers.json").json()['query']['dateRanges'][0]['startDate'])
+
+# Example 3:
+# BeautifulSoup - external module, using this we can handle html tags, classes and id.
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup 
+
+# html = urlopen('https://www.wikipedia.org')
+# bs = BeautifulSoup(html, 'html.parser')
+# print(bs)
+
+# To get text from some class:
+
+# nameList = bs.findAll('a', {'class':'link-box'})
+# for name in nameList:
+#   print(name.get_text())
+
+# nameList = bs.findAll('a', {'class':'jsl10n'})
+# for name in nameList:
+#   print(name.get_text())
+
+# nameList = bs.findAll('p', {'class':'jsl10n'})
+# for name in nameList:
+#   print(name.get_text())
+
+# import re
+
+# html = urlopen('https://pythonscraping.com/pages/page3.html')
+# bs = BeautifulSoup(html, 'html.parser')
+# images = bs.find_all('img', {'src':re.compile('.jpg')})
+# for img in images:
+#   # print(images)
+#   # print(img)
+#   print(img['src']+'\n')
+
+# help('bs4')
+# import requests
+# req = requests.get('https://makeawebsitehub.com/social-media-sites/')
+# req = requests.get('https://buffer.com/library/social-media-sites/')
+# facelinks = [fa for fa in req if 'https://www.facebook.com/']
+# # print(facelinks)
+  
+# for fa in facelinks:
+#   print(fa)
+
+# Header info about the response
+# import requests
+# res = requests.get('https://www.python.org/')
+# print("Status: ", res.status_code)
+# print("Headers: ", res.headers)
+# print("URL: ", res.url)
+# print("History: ", res.history)
+# print("Encoding: ", res.encoding)
+# print("Cookies: ", res.cookies)
+# print("Content: ", res._content)
+
+
+import requests
+from bs4 import BeautifulSoup
+
+# Printing Date, Time and Topic
+# res = requests.get('https://www.python.org/')
+# soup = BeautifulSoup(res.text, "html.parser")
+# upcoming = soup.find("div", class_="shrubbery")
+# events = upcoming.findAll("li")
+
+# for event in events:
+#   # print(event)
+#   # data = event.find('time')['datetime'].text.strip()
+#   # title = event.find("a").text.strip()
+#   # print(data + "-" + title)
+#   print(event.find('time')['datetime'], "-", event.find("a").text.strip())
+
+# Printing Headlines 
+# res = requests.get('https://www.python.org/')
+# soup = BeautifulSoup(res.text, "html.parser")
+# headlines = soup.find_all('ul', class_='menu')
+# for hl in headlines:
+#   print(hl.text.strip())
+
+# Printing Headers
+# import requests
+# res = requests.get('https://www.python.org/psf/donations/')
+# soup = BeautifulSoup(res.text, "html.parser")
+# headings = soup.findAll(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+# texts = [he.get_text() for he in headings]
+# for txt in texts:
+#   print(txt.strip())
+
+# Printing anchor tags' text
+# import requests
+# res = requests.get('https://www.python.org/psf/donations/')
+# soup = BeautifulSoup(res.text, "html.parser")
+# headings = soup.findAll(['a'])
+# texts = [he.get_text() for he in headings]
+# for txt in texts:
+#   print(txt.strip())
+
+# Printing title paragraph content, length
+# import requests
+# res = requests.get('https://www.python.org/psf/donations/')
+# soup = BeautifulSoup(res.text, "html.parser")
+# print(soup.title)
+# print(soup.findAll('p'))
+# print(len(soup.findAll('p')))
+# print(soup.find(href="https://google.com"))
+
